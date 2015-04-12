f5d2485b64405423681fb158367443a8fee6ddde
Kunal <kunal.bhagawati@gmail.com>
2015-04-12 17:49:37 +0530

    Added main file

--- game.py
+++ game.py
@@ -3,5 +3,8 @@ from core.components.board import Board
 
 if __name__ == "__main__":
     b = Board()
-    print("board string: ")
-    print(b.print_broard())
+    print()
+    print("isWhiteInCheck: {0}\n".format(b.is_white_in_check()))
+    print("board string:")
+    b.print_broard()
+    print()
